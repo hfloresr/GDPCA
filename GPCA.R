2df38ea35bdb903474877c8c270767592ab6584b
Hector Flores <hfloresr@uci.edu>
2017-07-20 17:15:59 +0300

    added gdpca

--- GPCA.R
+++ GPCA.R
@@ -13,73 +13,42 @@
 
 library(R.matlab)
 
-init_f <- function(z, k){
-
-  n = ncol(z)
+init_f <- function(Z, k){
+  n = ncol(Z)
   f_init = matrix(0, n + k, 1)
-  z = t(z)
+  Z_t = t(Z)
 
-  mzt = rowMeans(z)
+  mzt = rowMeans(Z_t)
   mzt = as.matrix(mzt)
 
-  z_new = matrix(0, dim(z)[1], dim(z)[2])
+  Z_new = matrix(0, dim(Z_t)[1], dim(Z_t)[2])
 
-  for(i in 1:dim(z)[2]){
-
-    z_new[, i] = z[, i] - mzt
-
+  for(i in 1:dim(z)[2]) {
+    Z_new[, i] = Z_t[, i] - mzt
   }
 
   #z_new = sweep(z, 2, rowMeans(z), "-")
-  z_svd = svd(z_new)
-  f_init[1:n, ] = z_new %*% as.matrix(z_svd$v[, 1])
-  #d_m = vec2diag(z_svd$d)
-  #temp_m = z_svd$u %*% d_m
-  #temp_m = as.matrix(temp_m[, 1])
-
-  #for(i in 1:n) {
-
-  # f_init[i,1] = temp_m[i, 1]
-
-  #}
+  Z_svd = svd(z_new)
+  f_init[1:n, ] = Z_new %*% as.matrix(Z_svd$v[, 1])
 
   f_init = f_init/sd(f_init)
-
-  return(f_init)
+  return (f_init)
 }
 
-dynamic_principal_components <- function() {
+
+run_gdpc <- function(Z, k, f_ini=NULL, tol=1e-04, maxiter=100) {
+  # Each ROW is a DIFFERENT time series
   # run the DPC model
-
-  # set up
-
-  #z = matrix(rnorm(10*100,mean=0,sd=1), 10, 100)
-  #zdata = readMat("z.mat")
-  z = readMat('F141020-lfp-5min-1kHz.mat')
-  pre_pmcao = z$pre.pmcao
-  z = pre_pmcao[((1-1)*1000+1):(1*1000), ]
-  z = t(z)
-  k1 = 50;
-  k2 = 30;
-  k = k1 + k2;
   m = nrow(z);
   T = ncol(z);
-  f_init = matrix(rnorm((T+k)*1,mean=0,sd=1), T + k, 1)
-  #f_initdata = readMat("f.mat")
-  #f_init = matrix(0, T + k, 1)
-  alpha_init = NaN;
-  beta_init = NaN;
-
-  # train
-  #f = init_f(z, k);
-  f = f_init;
-  alpha = alpha_init;
-  beta = beta_init;
-  train_iterations = 20;
-  loss_values = matrix(0, nrow=train_iterations, ncol=1)
 
 
-  for (train_iter in 1:train_iterations) {
+  f = ifelse(is.null(f_ini), init_f(Z, k), f_ini)
+  alpha = NaN
+  beta = NaN
+  loss_values = matrix(0, nrow=maxiter, ncol=1)
+
+  for (train_iter in 1:maxiter) {
     a = run_train_step(z, k, f, alpha, beta);
     f = a$f_new;
     alpha = a$alpha_new;
@@ -87,14 +56,12 @@ dynamic_principal_components <- function() {
     loss_values[train_iter] = evaluate_op(z, k, f, alpha, beta);
   }
 
-  # plot result
+  ## plot result
+  #plot(loss_values, lwd = 1.5, main="train loss (reconstruction MSE)", xlab = "iteration");
+  #print(loss_values)
 
-  plot(loss_values, lwd = 1.5, main="train loss (reconstruction MSE)", xlab = "iteration");
-  print(loss_values)
-
-
-  plot(f, lwd = 1.5, main = "Factor", xlab = "time" );
-  lines(f, lty = 1)
+  #plot(f, lwd = 1.5, main = "Factor", xlab = "time" );
+  #lines(f, lty = 1)
 }
 
 # ---------- below are all helper functions -------------------------------
@@ -110,103 +77,64 @@ run_train_step <- function(z, k, f, alpha, beta) {
   f_star = f_alpha_beta(z, k, alpha_new, beta_new);
   f_centered = f_star -  mean(f_star);
   f_new = sqrt(T + k) * f_centered / max(svd(f_centered)$d); ########
-
   return(list("f_new"=f_new, "alpha_new" = alpha_new, "beta_new" = beta_new))
-
 }
 
 
-evaluate_op <-function(z, k, f, alpha, beta){
+evaluate_op <- function(z, k, f, alpha, beta){
   loss = mean_squared_error(z, k, f, alpha, beta)
-  return(loss)
+  return (loss)
 }
 
 
-C_alpha <- function(z, alpha, k){
-  m = nrow(z);
-  T = ncol(z);
-  C = array(0, dim=c(m, T + k, k + 1));
-  for (j in 1:m){
-    for (t in 1:dim(C)[2]){
-      for (q in 1:dim(C)[3]){
-        if ((q >= max(1, t - T + 1)) && (q <= min(k + 1, t))){
-          C[j, t, q] = z[j, t - q + 1] - alpha[j]; #####
-        }
+C_alpha <- function(z_j, alpha_j, k){
+  T = ncol(z_j);
+  C = matrix(0, T + k, k + 1);
+  for (t in 1:(T + k)){
+    for (q in 1:(k + 1)){
+      if ((q >= max(1, t - T + 1)) && (q <= min(k + 1, t))){
+        C[t, q] = z[t - q + 1] - alpha_j
       }
     }
   }
-  return(C)
+  return (C)
 }
 
 
-D_beta <- function(rbeta, T, k) {
-  d_mat = array(0, dim=c(T+k, T+k))
+D_beta <- function(beta_j, T, k) {
+  # Construct matrix D w.r.t beta_j, T, and k
+  # beta_j is a row vector of the Beta matrix
+  D = matrix(0, T+k, T+k)
   for (t in 1:T+k) {
     for (r in max(t-k, 1):min(t,T)) {
       for(q in r:r+k) {
-        d_mat[t,q] = d_mat[t,q] + rbeta[q-r] %*% rbeta[t-r]
+        D[t,q] = D[t,q] + beta_j[q-r+1] %*% beta_j[t-r+1]
       }
     }
   }
-  return (beta_mat)
+  return (D)
 }
-
-#D_beta = function(z, beta, k){
-#  m = dim(z)[1]
-#  T = dim(z)[2]
-#  D = array(0,dim=c(m,T + k,T + k))
-#  for (j in 1:m) {
-#    for (t in 1:dim(D)[2]){
-#      for (q in 1:dim(D)[3]){
-#        if ((q >= max(t - k, 1)) && (q <= min(t + k, T + k))) {
-#          for (v in max(1, t - k, q - k):min(t, q, T)){
-#            D[j, t, q] = D[j, t, q] + beta[j, q - v + 1] * beta[j, t - v + 1];
-#          }
-#        }
-#      }
-#    }
-#  }
-#
-#  return(D)
-#}
 
 
 f_alpha_beta <- function(Z, alpha, beta, k){
+  # Get optimal f wrt Z, beta, alpha, and k
   m = nrow(Z)
   T = ncol(Z)
-  D = array(0, dim=c(T+k, T+k))
-  f = array(0, dim=c(T+k, 1))
+  D = matrix(0, T+k, T+k)
+  f = matrix(0, T+k, 1)
   for (j in 1:m) {
     D = D + D_beta(beta[j,], T, k)
     f = f + C_alpha(Z[j,], alpha[j]) %*% beta[j,]
   }
   f <- ifelse(rcond(D)>1e-10, solve(D,f), pinv(D))
+  return (f)
 }
 
 
-#f_alpha_beta <- function(z, k, alpha, beta){
-#  m = nrow(z);
-#  T = ncol(z);
-#  D_3d = D_beta(z, beta, k);
-#  D = drop(colSums(D_3d));
-#  C_3d = C_alpha(z, alpha, k);
-#  sum_Cj_betaj = 0;
-#  for (j in 1:m){
-#    beta_v = c(beta[j, ])
-#    beta_t = t(beta_v)
-#    beta_tt = t(beta_t)
-#    sum_Cj_betaj = sum_Cj_betaj + drop(C_3d[j, , ]) %*% Conj(beta_tt);
-#
-#  }
-#
-#  f = solve(D, sum_Cj_betaj);
-#  return(f);
-#}
-
-
-F_f <- function(z, f, k){
-  m = nrow(z);
-  T = ncol(z);
+F_f <- function(Z, f, k){
+  # Gets F matrix s.t. F(f) is T x (k+2) w/ t-th row (f_t, f_t+1,...,f_t+k, 1)
+  m = nrow(Z);
+  T = ncol(Z);
   F = matrix(0,nrow=T,ncol=k+2)
   for (t in 1:nrow(F)){
     F[t, ] = cbind(Conj(t(f[t:(t + k)])), 1);
@@ -214,18 +142,22 @@ F_f <- function(z, f, k){
   return(F);
 }
 
-alpha_beta <- function(z, f, k){
-  m=nrow(z)
-  T=ncol(z)
-  F=F_f(z, f, k)
+
+alpha_beta <- function(Z, f, k){
+  # Find the optimal beta
+  # input: Z data matrix, f-principal components, k-leads
+  # output: beta matrix of loadings, alpha-intercepts
+  m=nrow(Z)
+  T=ncol(Z)
+  F=F_f(Z, f, k)
   FtF_inv = solve(Conj(t(F)) %*% F);
   FtF_inv_Ft = FtF_inv %*% Conj(t(F));
   tmp = z %*% Conj(t(FtF_inv_Ft));
   alpha = Conj(t(tmp[, k + 2]));
   beta = tmp[, 1:(k + 1)];
+  #res = Z - beta %*% F
   return(list("alpha"=alpha, "beta"=beta))
 }
-
 
 
 mean_squared_error <- function(z, k, f, alpha, beta){
@@ -242,8 +174,17 @@ mean_squared_error <- function(z, k, f, alpha, beta){
     }
   }
   err = sum_squared_error / (T * m);
-
   return(err);
 }
 
-dynamic_principal_components()
+
+
+#z = matrix(rnorm(10*100,mean=0,sd=1), 10, 100)
+#zdata = readMat("z.mat")
+Z = readMat('F141020-lfp-5min-1kHz.mat')
+pre_pmcao = z$pre.pmcao
+Z = pre_pmcao[((1-1)*1000+1):(1*1000), ]
+Z_t = t(z)
+k=2
+
+run_gdpc(Z_t, k, f_ini=NULL, tol=1e-04, maxiter=1000)
